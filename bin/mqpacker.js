e2fb8d60b96f779c5523335151890a9ef559b0d5
Shobhit Sharma <772855+shobhitsharma@users.noreply.github.com>
2020-07-21 22:22:05 +0200

    refactor: ran prettier

--- bin/mqpacker.js
+++ bin/mqpacker.js
@@ -6,23 +6,23 @@ const mqpacker = require("../index");
 const pkg = require("../package.json");
 
 const argv = minimist(process.argv.slice(2), {
-	boolean: ["help", "sort", "sourcemap", "version"],
-	alias: {
-		h: "help",
-		s: "sort"
-	},
-	default: {
-		help: false,
-		sourcemap: false,
-		sort: false,
-		version: false
-	}
+  boolean: ["help", "sort", "sourcemap", "version"],
+  alias: {
+    h: "help",
+    s: "sort",
+  },
+  default: {
+    help: false,
+    sourcemap: false,
+    sort: false,
+    version: false,
+  },
 });
 const [binname] = Object.keys(pkg.bin);
 const options = {};
 
 const showHelp = () => {
-	console.log(`Usage: ${binname} [options] INPUT [OUTPUT]
+  console.log(`Usage: ${binname} [options] INPUT [OUTPUT]
 
 Description:
   ${pkg.description}
@@ -41,72 +41,72 @@ Examples:
 };
 
 const pack = (s, o) => {
-	mqpacker
-		.pack(s, o)
-		.then((result) => {
-			if (!o.to) {
-				process.stdout.write(result.css);
-				return;
-			}
+  mqpacker
+    .pack(s, o)
+    .then((result) => {
+      if (!o.to) {
+        process.stdout.write(result.css);
+        return;
+      }
 
-			fs.writeFileSync(o.to, result.css);
+      fs.writeFileSync(o.to, result.css);
 
-			if (result.map) {
-				fs.writeFileSync(`${o.to}.map`, result.map);
-			}
-		})
-		.catch((error) => {
-			if (error.name !== "CssSyntaxError") {
-				throw error;
-			}
+      if (result.map) {
+        fs.writeFileSync(`${o.to}.map`, result.map);
+      }
+    })
+    .catch((error) => {
+      if (error.name !== "CssSyntaxError") {
+        throw error;
+      }
 
-			process.exitCode = 1;
-			console.error(
-				`${error.file}:${error.line}:${error.column}: ${error.reason}`
-			);
-		});
+      process.exitCode = 1;
+      console.error(
+        `${error.file}:${error.line}:${error.column}: ${error.reason}`
+      );
+    });
 };
 
 if (argv._.length < 1) {
-	argv.help = true;
+  argv.help = true;
 }
 
 switch (true) {
-	case argv.version:
-		console.log(`${binname} v${pkg.version}`);
-		break;
+  case argv.version:
+    console.log(`${binname} v${pkg.version}`);
+    break;
 
-	case argv.help:
-		showHelp();
-		break;
+  case argv.help:
+    showHelp();
+    break;
 
-	default: {
-		if (argv.sort) {
-			options.sort = true;
-		}
+  default: {
+    if (argv.sort) {
+      options.sort = true;
+    }
 
-		if (argv.sourcemap) {
-			options.map = true;
-		}
+    if (argv.sourcemap) {
+      options.map = true;
+    }
 
-		[options.from, options.to] = argv._;
-		let input = options.from;
+    [options.from, options.to] = argv._;
+    let input = options.from;
 
-		if (input === "-") {
-			delete options.from;
-			input = process.stdin.fd;
-		}
+    if (input === "-") {
+      delete options.from;
+      input = process.stdin.fd;
+    }
 
-		if (!options.to) {
-			delete options.to;
-		}
+    if (!options.to) {
+      delete options.to;
+    }
 
-		if (options.map && options.to) {
-			options.map = {
-				inline: false
-			};
-		}
+    if (options.map && options.to) {
+      options.map = {
+        inline: false,
+      };
+    }
 
-		pack(fs.readFileSync(input, "utf8"), options);
-	}
+    pack(fs.readFileSync(input, "utf8"), options);
+  }
 }
